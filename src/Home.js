676da5d5e48980472a0236e0941ef09a275b813c
anamariadanila <ana.danila@info.uaic.ro>
2022-04-08 12:49:38 +0300

    routes created

--- src/Home.js
+++ src/Home.js
@@ -1,40 +1,14 @@
-import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
+import useFetch from "./useFetch";
 
 const Home = () => {
-  const [blogs, setBlogs] = useState(null);
-  const [isPending, setIsPending] = useState(true);
-  const [error, setError] = useState(null);
-
-  useEffect(() => {
-    setTimeout(
-      () =>
-        fetch("http://localhost:8090/blogs")
-          .then((res) => {
-            if (!res.ok) {
-              throw Error("Could not fetch the data");
-            }
-            return res.json();
-          })
-          .then((data) => {
-            console.log(data);
-            setBlogs(data);
-            setIsPending(false);
-            setError(null);
-          })
-          .catch((err) => {
-            setIsPending(false);
-            setError(err.message);
-          }),
-      500
-    );
-  }, []);
+  const { data, isPending, error } = useFetch("http://localhost:8090/blogs");
 
   return (
     <div className="home">
       {error && <div>{error}</div>}
       {isPending && <div>Loading...</div>}
-      {blogs && <BlogList blogs={blogs} title="All Blogs" />}
+      {data && <BlogList blogs={data} />}
     </div>
   );
 };
